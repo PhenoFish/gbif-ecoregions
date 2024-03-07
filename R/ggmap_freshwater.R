5de4e2c52067ce7bce2c06b53f4982f6cc7576e3
Nicolas Casajus <ahasverus@users.noreply.github.com>
2024-03-07 08:26:07 +0100

    factor: generalise ggmap_*() functions

--- R/ggmap_freshwater.R
+++ R/ggmap_freshwater.R
@@ -1,15 +1,53 @@
-#' Map number of freshwater fish species per World drainage basin
+#' Map spatial object at World drainage basins level
+#' 
+#' @description
+#' Represent a spatial metric on a World map at the drainage basins scale. By 
+#' default, the number of freshwater fish species per World drainage basin is 
+#' mapped.
 #'
+#' @param file a `character` of length 1. The relative path to the spatial 
+#'   object to read. Default is `outputs/phenofish_freshwater_richness.gpkg`.
+#' 
+#' @param metric the column in `file` to map. The name of the column be 
+#'   unquoted (`metric = richness` and not `metric = "richness"`).
+#'   Default is `richness`.
+#' 
+#' @param title a `character` of length 1. The title of the map. Default is
+#'   `Number of freshwater fish species`.
+#' 
 #' @return A `ggplot` object.
 #' 
 #' @export
-#'
+#' 
 #' @examples
 #' ## ggmap_freshwater()
 
-ggmap_freshwater <- function() {
+ggmap_freshwater <- function(
+    file = here::here("outputs", "phenofish_freshwater_richness.gpkg"), 
+    metric = richness, 
+    title = "Number of freshwater fish species") {
   
-  ## Read map data ----
+  
+  ## Check args ----
+  
+  if (!is.character(file) && length(file) != 1) {
+    stop("Argument 'file' must a character of length 1", call. = FALSE)
+  }
+  
+  if (!file.exists(file)) {
+    stop("The file '", file, "' does not exist", call. = FALSE)
+  }
+  
+  # if (!is.character(metric) && length(metric) != 1) {
+  #   stop("Argument 'metric' must a character of length 1", call. = FALSE)
+  # }
+  
+  if (!is.character(title) && length(title) != 1) {
+    stop("Argument 'title' must a character of length 1", call. = FALSE)
+  }
+  
+  
+  ## Read base map layers ----
   
   ne_oceans     <- sf::st_read(here::here("data", "basemap", 
                                           "ne_oceans.gpkg"), quiet = TRUE)
@@ -26,10 +64,14 @@ ggmap_freshwater <- function() {
   
   ## Read richness data ----
   
-  data_sf <- sf::st_read(here::here("outputs", 
-                                    "phenofish_freshwater_richness.gpkg"),
-                         quiet = TRUE)
+  data_sf <- sf::st_read(file, quiet = TRUE)
   
+  # if (!(metric %in% colnames(data_sf))) {
+  #   stop("The column '", metric, "'is not found in '", file, "'", call. = FALSE)
+  # }
+  
+  
+  ## Make map ----
   
   ggplot2::ggplot() +
     
@@ -45,13 +87,13 @@ ggmap_freshwater <- function() {
     ggplot2::geom_sf(data = ne_bbox, fill = NA, col = "#a6a6a6", 
                      linewidth = 0.10) +
     
-    ggplot2::geom_sf(data = data_sf, ggplot2::aes(fill = richness)) +
+    ggplot2::geom_sf(data = data_sf, ggplot2::aes(fill = {{ metric }})) +
     
     ggplot2::theme_void() +
     ggplot2::theme(legend.position  = "bottom",
-                   legend.key.width = ggplot2::unit(1.5, 'cm'),
+                   legend.key.width = ggplot2::unit(1.5, "cm"),
                    legend.title = ggplot2::element_text(face = "bold")) +
-    ggplot2::labs(fill = "Number of freshwater fish species") + 
+    ggplot2::labs(fill = title) + 
     ggplot2::guides(fill = ggplot2::guide_colorbar(title.position = "top", 
                                                    title.hjust = 0.5))
 }
--- R/ggmap_marine.R
+++ R/ggmap_marine.R
@@ -1,20 +1,53 @@
-#' Map number of marine fish species per World ecoregion
-#'
+#' Map spatial object at World ecoregions level
+#' 
+#' @description
+#' Represent a spatial metric on a World map at the marine ecoregions 
+#' scale. By default, the number of marine fish species per World ecoregion is 
+#' mapped.
+#' 
+#' @param file a `character` of length 1. The relative path to the spatial 
+#'   object to read. Default is `outputs/phenofish_marine_richness.gpkg`.
+#' 
+#' @param metric the column in `file` to map. The name of the column be 
+#'   unquoted (`metric = richness` and not `metric = "richness"`).
+#'   Default is `richness`.
+#' 
+#' @param title a `character` of length 1. The title of the map. Default is
+#'   `Number of marine fish species`.
+#' 
 #' @return A `ggplot` object.
 #' 
 #' @export
-#' @file
-#' @metric
+#' 
 #' @examples
 #' ## ggmap_marine()
 
-ggmap_marine <- function(file,metric, title) {
+ggmap_marine <- function(
+    file = here::here("outputs", "phenofish_marine_richness.gpkg"), 
+    metric = richness, 
+    title = "Number of marine fish species") {
   
-  #title = "Number of marine fish species"
-  #file =
-  #metric =
   
-  ## Read map data ----
+  ## Check args ----
+  
+  if (!is.character(file) && length(file) != 1) {
+    stop("Argument 'file' must a character of length 1", call. = FALSE)
+  }
+  
+  if (!file.exists(file)) {
+    stop("The file '", file, "' does not exist", call. = FALSE)
+  }
+  
+  # if (!is.character(metric) && length(metric) != 1) {
+  #   stop("Argument 'metric' must a character of length 1", call. = FALSE)
+  # }
+  
+  if (!is.character(title) && length(title) != 1) {
+    stop("Argument 'title' must a character of length 1", call. = FALSE)
+  }
+  
+  
+  ## Read base map layers ----
   
   ne_oceans     <- sf::st_read(here::here("data", "basemap", 
                                           "ne_oceans.gpkg"), quiet = TRUE)
@@ -29,15 +62,22 @@ ggmap_marine <- function(file,metric, title) {
                                                      "Antarctica"), ]
   
   
-  ## Read richness data ----
+  ## Read user data ----
   
-  data_sf <- sf::st_read(here::here("outputs", 
-                                    file), 
-                         quiet = TRUE)
+  data_sf <- sf::st_read(file, quiet = TRUE)
 
+  # if (!(metric %in% colnames(data_sf))) {
+  #   stop("The column '", metric, "'is not found in '", file, "'", call. = FALSE)
+  # }
+  
+  
+  ## Correct and convert data ----
+  
   data_sf <- correct_meow(data_sf)
   data_sf <- sf::st_transform(data_sf, crs = sf::st_crs(ne_bbox))
   
+  
+  ## Make map ----
   
   ggplot2::ggplot() +
     
@@ -46,7 +86,7 @@ ggmap_marine <- function(file,metric, title) {
     ggplot2::geom_sf(data = ne_graticules, col = "#bae2fb", 
                      linewidth = 0.10) +
     
-    ggplot2::geom_sf(data = data_sf, ggplot2::aes(fill = metric)) +
+    ggplot2::geom_sf(data = data_sf, ggplot2::aes(fill = {{ metric }})) +
     
     ggplot2::geom_sf(data = ne_countries, fill = "#c0c0c0", col = "#c9c9c9", 
                      linewidth = 0.10) +
@@ -57,10 +97,9 @@ ggmap_marine <- function(file,metric, title) {
     
     ggplot2::theme_void() +
     ggplot2::theme(legend.position  = "bottom",
-                   legend.key.width = ggplot2::unit(1.5, 'cm'),
+                   legend.key.width = ggplot2::unit(1.5, "cm"),
                    legend.title = ggplot2::element_text(face = "bold")) +
     ggplot2::labs(fill = title) + 
     ggplot2::guides(fill = ggplot2::guide_colorbar(title.position = "top", 
-                                                 title.hjust = 0.5))
-    
+                                                   title.hjust = 0.5))
 }
